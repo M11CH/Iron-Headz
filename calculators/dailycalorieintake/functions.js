6b55bab8c1a37f6ddb922517d2a4b393ff52f385
Mohamad Issawi <mo@dragona.ca>
2018-02-02 16:53:28 -0500

    Result window displayed when submit button is clicked, results row is cleared if it exists when clear button is pressed

--- calculators/dailycalorieintake/functions.js
+++ calculators/dailycalorieintake/functions.js
@@ -1,3 +1,25 @@
 function clearform() {
+    // return input fields to original state
     document.getElementById("calculator").reset();
+
+    // enable submit button
+    document.getElementById("submitbutt").disabled=false;
+
+    // delete results row if it exists
+    var count = document.getElementById("calculatortab").getElementsByTagName("tr").length;
+    if (count==16) {
+	document.getElementById("calculatortab").deleteRow(-1);
+    }
+}
+
+function displayresult() {
+    // display calculation result
+    var table = document.getElementById("calculatortab");
+    var row = table.insertRow(-1);
+    var cell = row.insertCell(0);
+    cell.innerHTML = "RESULT";
+
+    // disable submit button
+    document.getElementById("submitbutt").disabled=true;
+
 }
